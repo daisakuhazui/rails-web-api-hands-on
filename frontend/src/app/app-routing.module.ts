d51f8635e3e56fc806bf8d5a382115d018e82050
DaisakuHazui <h.daisaku.biwabiwako@gmail.com>
2018-12-25 19:43:28 +0900

    Add TodoComponent routing to app-routing.module.ts

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TodoComponent } from './component/todo/todo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'todos',
+    component: TodoComponent,
+    children: []
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
